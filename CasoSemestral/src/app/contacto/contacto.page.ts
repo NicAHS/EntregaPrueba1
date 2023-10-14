14b9a23f31cc311519b08a6658cee900bd102f1d
Nicolás <ni.hinojosa@duocuc.cl>
2023-10-14 20:32:02 -0300

    14-10-2023
    Algunos detalles

--- CasoSemestral/src/app/contacto/contacto.page.ts
+++ CasoSemestral/src/app/contacto/contacto.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { IonTextarea } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-contacto',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router,private alertController: AlertController) { }
+  
+  @ViewChild('Texto', { static: false }) Texto!: IonTextarea;
+  
   ngOnInit() {
+  }
+
+  async enviarFormulario(){
+    const texto = this.Texto.value;
+    if (!texto || texto.trim() === '') {
+      const alert = await this.alertController.create({
+        header: 'Advertencia',
+        message: 'El área de texto está vacía. Por favor rellene el formulario antes de enviar',
+        buttons: ['Aceptar']
+      });
+      await alert.present();
+      return; 
+    }else{
+      const alert = await this.alertController.create({
+        header: 'Mensaje',
+        message: 'El formulario ha sido enviado correctamente.',
+        buttons: ['Aceptar']
+      });
+      await alert.present();
+      this.router.navigate(["/usuario"]);
+      return;
+    }
   }
 
 }
